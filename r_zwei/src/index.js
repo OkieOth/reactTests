df90c0ba47d621a257e0737237a64abbee7e802d
okieoth <eiko.thomas@gmx.net>
2017-01-18 09:24:36 +0100

    [r_zwei] modify webpack devel config for hot reload in case of 404

--- r_zwei/src/index.js
+++ r_zwei/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route} from 'react-router';
-import { createHashHistory } from 'history';
-//import { Router, Route,browserHistory} from 'react-router';
+import { Router, Route,browserHistory} from 'react-router';
 import App from './App';
 import Link1 from './Link1';
 import Link2 from './Link2';
@@ -10,9 +8,7 @@ import Link3 from './Link3';
 
 import './index.css';
 
-const history = createHashHistory();
-
-const router = <Router history={history}>
+const router = <Router history={browserHistory}>
         <Route path='/' component={App} />
         <Route path='/link1' component={Link1} />
         <Route path='/link2' component={Link2} />
