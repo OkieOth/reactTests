095e400d3c5e84020b237eb155573a46c9112fb7
OkieOth <okie.othsam@gmail.com>
2017-01-22 21:43:17 +0100

    [r_vier] redux works

--- r_vier/src/App.tsx
+++ r_vier/src/App.tsx
@@ -1,65 +1,80 @@
 import React, {} from 'react';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import {getCLICKED_1,getCLICKED_2, getCLICKED_3} from './actions/App_actions';
-
 import {IState} from "./actions/action_types";
 
 const style = require("./App.css");
 const logo = require("./logo.svg");
 
 interface IAppProps extends React.Props<any> {
-    getCLICKED_1: () => void;
-    getCLICKED_2: () => void;
-    getCLICKED_3: () => void;
-};
+    getCLICKED_1x: () => void;
+    getCLICKED_2x: () => void;
+    getCLICKED_3x: () => void;
+}
 
 function mapStateToProps(state: IState) {
+    console.log("mapStateToProps: state"+state.clickCount1);
     return {
-        clickCount1: state.clickCount1,
-        clickCount2: state.clickCount2,
-        clickCount3: state.clickCount3
+            clickCount1: state.clickCount1,
+            clickCount2: state.clickCount2,
+            clickCount3: state.clickCount3
     };
 }
 
 function mapDispatchToProps(dispatch) {
     return {
-        getCLICKED_1: () => dispatch(getCLICKED_1()),
-        getCLICKED_2: () => dispatch(getCLICKED_2()),
-        getCLICKED_3: () => dispatch(getCLICKED_3())
+        getCLICKED_1x: () => {
+                console.log("getClicked1");
+                const action = getCLICKED_1();
+                dispatch(action)
+        },
+        getCLICKED_2x: () => {
+            console.log("getClicked2");
+            const action = getCLICKED_2();
+            dispatch(action)
+        },
+        getCLICKED_3x: () => {
+            console.log("getClicked3");
+            const action = getCLICKED_3();
+            dispatch(action)
+        }
     };
 }
 
 
 export class App extends React.Component<IAppProps, void> {
+    printValue (place, count ) :number {
+        console.log("printValue: "+place+", count="+count);
+        return count;
+    }
     render() {
-        const { clickCount1,clickCount2,clickCount3,getCLICKED_1,getCLICKED_2,getCLICKED_3 } = this.props;
+        const { clickCount1,clickCount2,clickCount3,getCLICKED_1x,getCLICKED_2x,getCLICKED_3x } = this.props;
 
         return (
             <div className="App">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to React r_vier</h2>
+                    <h2>Welcome to React r_vier :)</h2>
                 </div>
                 <div className="btns">
-                    <button onClick={ getCLICKED_1 }>ONE</button>
-                    <button onClick={ getCLICKED_2 }>TWO</button>
-                    <button onClick={ getCLICKED_3 }>THREE</button>
+                    <button onClick={ getCLICKED_1x }>ONE</button>
+                    <button onClick={ getCLICKED_2x }>TWO</button>
+                    <button onClick={ getCLICKED_3x }>THREE</button>
                 </div>
                 <div>
                     <div>
                         <p>
                             <span>Clicks on Button 1:</span>
-                            <span>{clickCount1}</span>
+                            <span>{ this.printValue("clickCount1",clickCount1) }</span>
                         </p>
                         <p>
                             <span>Clicks on Button 2:</span>
-                            <span>{clickCount2}</span>
+                            <span>{ this.printValue("clickCount2",clickCount2) }</span>
                         </p>
                         <p>
                             <span>Clicks on Button 3:</span>
-                            <span>{clickCount3}</span>
+                            <span>{ this.printValue("clickCount3",clickCount3) }</span>
                         </p>
                     </div>
                 </div>
--- r_vier/src/reducers/App_reducer.ts
+++ r_vier/src/reducers/App_reducer.ts
@@ -9,22 +9,33 @@ const initialState = {clickCount1:0, clickCount2: 0, clickCount3: 0};
 const appReducer = (state: ActionTypes.IState = initialState, action : ActionTypes.IAction ): ActionTypes.IState => {
     switch (action.type) {
         case ActionTypes.CLICKED_1:
-            state.clickCount1++;
-            return state;
+            console.log("reducer: clicked_1: clickCount="+state.clickCount1);
+            return { clickCount1: state.clickCount1 + 1,
+                        clickCount2: state.clickCount2,
+                        clickCount3: state.clickCount3
+            };
         case ActionTypes.CLICKED_2:
-            state.clickCount2++;
-            return state;
+            console.log("reducer: clicked_2: clickCount="+state.clickCount2);
+            return { clickCount1: state.clickCount1,
+                clickCount2: state.clickCount2 + 1,
+                clickCount3: state.clickCount3
+            };
         case ActionTypes.CLICKED_3:
-            state.clickCount3++;
-            return state;
+            console.log("reducer: clicked_3: clickCount="+state.clickCount3);
+            return { clickCount1: state.clickCount1,
+                clickCount2: state.clickCount2,
+                clickCount3: state.clickCount3 +1
+            };
         default:
+            console.log("reducer: default");
             return state;
     }
 };
 
-/*
+/* don't work this time :-/
 export const store = createStore(combineReducers({
-    app: appReducer
+    App: appReducer
 }));
 */
+
 export const store = createStore(appReducer);
